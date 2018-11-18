aff5757184edbfa1effa42a6d6bfd228da6e17cb
Alex Witherspoon <Alex@AlexWitherspoon.com>
2018-11-18 09:47:05 -0800

    Added Path, and logging

--- opt/UpdateMattermost.sh
+++ opt/UpdateMattermost.sh
@@ -2,8 +2,14 @@
 # https://github.com/alexwitherspoon/spoon.family
 # Update Mattermost Automatically
 
-cd /opt/mattermost-docker
+echo "`date +"%T-%m-%d-%Y"`: Starting Mattermost Upgrade"
+cd /opt/mattermost-docker/
+echo "`date +"%T-%m-%d-%Y"`: Shutting down existing Mattermost"
 docker-compose down
-git pull
+echo "`date +"%T-%m-%d-%Y"`: Updating Git Repo"
+/usr/bin/git pull
+/usr/bin/git status
+echo "`date +"%T-%m-%d-%Y"`: Building Mattermost Containers"
 docker-compose build
+echo "`date +"%T-%m-%d-%Y"`: Starting new Mattermost Containers"
 docker-compose up -d
