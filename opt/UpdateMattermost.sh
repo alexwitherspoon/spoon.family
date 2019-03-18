4ef14dccf241779c2d92be4d7de1b5e75ead419c
Alex Witherspoon <Alex@AlexWitherspoon.com>
2019-03-18 10:00:46 -0700

    Preserve docker-compose file
    This preserves our custom docker compose file between git-pulls.

--- opt/UpdateMattermost.sh
+++ opt/UpdateMattermost.sh
@@ -7,8 +7,10 @@ cd /opt/mattermost-docker/
 echo "`date +"%T-%m-%d-%Y"`: Shutting down existing Mattermost"
 docker-compose down
 echo "`date +"%T-%m-%d-%Y"`: Updating Git Repo"
+cp -v ./docker-compose.yml /tmp/docker-compose.yml
 /usr/bin/git pull
 /usr/bin/git status
+mv -v /tmp/docker-compose.yml ./docker-compose.yml
 echo "`date +"%T-%m-%d-%Y"`: Building Mattermost Containers"
 docker-compose build
 echo "`date +"%T-%m-%d-%Y"`: Starting new Mattermost Containers"
